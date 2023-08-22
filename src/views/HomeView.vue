40cf6e241ff9120c6f81ddec75a5d06d79ee1aec
Daniel Munoz <danimm.dev@gmail.com>
2023-08-22 15:06:52 +0200

    creating theme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ defineOptions({
 </script>
 
 <template>
-  <div>
+  <div class="container lg:container 2xl:container 4xl:container">
     <h1 class="text-2xl">Hello World</h1>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,35 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: '#C9A15B',
+      secondary: '#CECECE',
+      text: '#8C8C8C',
+      'hero-bg': '#808080',
+      'border-bg': '#1D1D1D',
+      'slider-bg': '#0F0F0F'
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1800px',
+      '3xl': '2200px',
+      '4xl': '2600px'
+    },
+    container: {
+      padding: '40px',
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        '2xl': '1800px',
+        '3xl': '2200px',
+        '4xl': '2600px'
+      }
+    },
     extend: {}
   },
   plugins: []
