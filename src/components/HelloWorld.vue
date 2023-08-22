6a462506ea1d4768711965fc9460b5a34d411e6a
Daniel Munoz <danimm.dev@gmail.com>
2023-08-22 11:45:28 +0200

    removing some stuff

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts" generic="T">
-defineProps<{ msg: T; msgList: T[] }>()
+<script setup lang="ts">
+defineProps<{ msg: string }>()
 </script>
 
 <template>
@@ -12,28 +12,3 @@ defineProps<{ msg: T; msgList: T[] }>()
     </h3>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
