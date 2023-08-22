819b2c3c756e78e1e7d60f7a68e10279fd3cf636
Daniel Munoz <danimm.dev@gmail.com>
2023-08-22 17:21:54 +0200

    slider card finish

--- src/components/SliderCard.vue
+++ src/components/SliderCard.vue
@@ -1,29 +1,31 @@
 <script lang="ts" setup>
-defineOptions({
-  name: 'SliderCard'
-})
+defineOptions({ name: 'SliderCard' })
 </script>
 
 <template>
-  <div class="w-[620px] mt-[100px]">
-    <div class="h-[800px] bg-black flex flex-col items-center pb-[112px]">
+  <div class="w-[620px] h-[900px] mt-[100px] bg-black">
+    <div class="pt-[19px] translate-y-[-100px]">
       <img
-        class="h-[540px] w-[280px] translate-y-[-100px] mb-[-100px]"
+        class="h-[531px] w-[277px] mx-auto"
         src="@/assets/images/img01.png"
         alt="Altiplano watch"
       />
-      <h4
-        class="pt-[54px] tracking-widest text-secondary leading-[19.2px] text-subheader font-trade-bold uppercase"
-      >
-        Piaget
-      </h4>
-      <h2 class="pt-[35px] tracking-wider text-white text-title font-minion text-5xl">Altiplano</h2>
-      <h4 class="pt-[20px] tracking-wider text-secondary text-[13pt ] font-trade-light">
-        Watch G0A39111
-      </h4>
-      <h3 class="pt-[58px] tracking-wider text-white text-[20pt] font-minion font-bold">
-        CHF 24'900.00
-      </h3>
+      <div class="mt-[60px] text-center">
+        <h4
+          class="mb-[35px] tracking-widest text-secondary text-subheader font-trade-bold uppercase"
+        >
+          Piaget
+        </h4>
+        <h2 class="mb-[22px] tracking-wider text-white text-title font-minion text-5xl">
+          Altiplano
+        </h2>
+        <h4 class="mb-[65px] tracking-wider text-secondary text-[13pt] font-trade-light">
+          Watch G0A39111
+        </h4>
+        <h3 class="pb-[17px] tracking-wider text-white text-[20pt] font-minion font-bold">
+          CHF 24'900.00
+        </h3>
+      </div>
     </div>
   </div>
 </template>
