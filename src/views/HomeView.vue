9db3b4f864b8cc80d8a09aea25dd54530ee52a0d
Daniel Munoz <danimm.dev@gmail.com>
2023-08-23 18:31:24 +0200

    remove unnsed components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import SliderCard from '@/components/SliderCard.vue'
-import ProductCharacteristics from '@/components/sections/Product/ProductCharacteristics.vue'
 import ProductsContainer from '@/components/sections/Product/ProductsContainer.vue'
 
-defineOptions({
-  name: 'HomeView'
-})
+defineOptions({ name: 'HomeView' })
 </script>
 
 <template>
   <ProductsContainer />
-  <!--  <SliderCard />-->
-  <!--  <ProductCharacteristics property="Metal" description="Rose gold / Silver" />-->
 </template>
