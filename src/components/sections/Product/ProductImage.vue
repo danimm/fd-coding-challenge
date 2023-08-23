bd8b18d37fc6e068788ab2892b9ed0d0bd99ed6f
Daniel Munoz <danimm.dev@gmail.com>
2023-08-23 18:37:57 +0200

    new prop and remove unnused lint script

--- src/components/sections/Product/ProductImage.vue
+++ src/components/sections/Product/ProductImage.vue
@@ -5,14 +5,16 @@ defineOptions({ name: 'ProductImage' })
 
 const props = defineProps({
   imageOverflow: { type: Boolean, default: false },
-  zoom: { type: Boolean, default: false }
+  zoom: { type: Boolean, default: false },
+  centered: { type: Boolean, default: false }
 })
 
 const zoomPercentage = computed(() => (props.zoom ? 'h-[120%]' : 'h-[100%]'))
 
 const imageDynamicStyles = computed(() => ({
   'translate-x-[-30px]': props.imageOverflow,
-  [zoomPercentage.value]: props.zoom
+  [zoomPercentage.value]: props.zoom,
+  'mx-auto': props.centered
 }))
 </script>
 
