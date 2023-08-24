f060800b7c7ab749a2defa19427b6117e1cf6cab
Daniel Munoz <danimm.dev@gmail.com>
2023-08-24 16:46:49 +0200

    update Navbar links

---
+++ src/components/core/navbar/NavBarLink.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+defineOptions({ name: 'NavBarLink' })
+
+withDefaults(defineProps<{ to?: string; activeRoute: boolean }>(), {
+  to: '#',
+  activeRoute: false
+})
+defineEmits<{ onClick: [] }>()
+</script>
+
+<template>
+  <a
+    :href="to"
+    class="transition-all no-underline text-black uppercase font-minion font-bold tracking-widest text-normal"
+    :class="{
+      'hover:scale-110 hover:text-primary ': !activeRoute
+    }"
+    @click.prevent="$emit('onClick')"
+  >
+    <span class="relative" :class="{ 'active-route': activeRoute }">
+      <slot />
+    </span>
+  </a>
+</template>
+
+<style scoped>
+.active-route {
+  @apply text-primary scale-110;
+}
+
+.active-route::before {
+  content: '';
+  position: absolute;
+  background-color: pink;
+  width: 120%;
+  height: 3px;
+  bottom: -10px;
+  left: -10px;
+  @apply bg-primary text-primary;
+}
+</style>
