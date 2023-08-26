4de10eb6d42c892b05265adf9efe89813c110a91
Daniel Munoz <danimm.dev@gmail.com>
2023-08-26 20:20:09 +0200

    hero until md

--- src/components/hero/HeroComponent.vue
+++ src/components/hero/HeroComponent.vue
@@ -20,13 +20,14 @@ defineEmits<{
 </script>
 
 <template>
-  <div class="h-[1970px] pt-[120px] pb-[140px] bg-hero-bg-light">
+  <div class="3xl:h-[1970px] pt-[120px] md:py-[120px] 3xl:pb-[140px] bg-hero-bg-light">
     <div class="container grid grid-cols-12 gap-[42px]">
-      <div class="col-span-6">
-        <div class="flex justify-between">
-          <div class="flex flex-col justify-center items-start">
+      <!-- Gallery -->
+      <div class="md:col-span-12 3xl:col-span-7 4xl:col-span-6">
+        <div class="md:grid md:grid-cols-6 lg:flex lg:justify-center 3xl:justify-between">
+          <div class="md:col-span-6 flex lg:flex-col justify-center items-start">
             <div
-              class="pb-[33px] cursor-pointer"
+              class="md:pr-[33px] lg:pb-[33px] cursor-pointer"
               v-for="(image, index) in gallery"
               :key="image"
               @click="$emit('updateSelectedImage', { index, image })"
@@ -35,41 +36,53 @@ defineEmits<{
                 <img
                   :src="getImageUrl(image)"
                   :alt="title"
-                  class="h-[240px] w-[222px] object-contain bg-white"
+                  class="md:h-[240px] w-[222px] object-contain bg-white"
                   :class="[selectedGalleryImageIndex === index ? 'opacity-100' : 'opacity-30']"
                 />
               </picture>
             </div>
           </div>
 
-          <picture>
+          <!-- Main Image -->
+          <picture class="md:col-span-6">
             <img
               :src="getImageUrl(mainImage)"
               :alt="title"
-              class="h-[1776px] max-w-[880px] w-full object-cover"
+              class="h-full md:h-[1000px] md:max-h-[1000px] 3xl:h-[1776px] 3xl:max-h-none 3xl:max-w-[880px] w-full object-cover"
             />
           </picture>
         </div>
       </div>
 
-      <div class="col-start-8 col-span-5 py-[212px]">
-        <div class="grid grid-cols-6">
-          <div class="col-span-5">
-            <h3 class="font-trade-bold text-normal text-primary pb-[78px] uppercase">
+      <!-- Description -->
+      <div class="md:col-span-12 3xl:col-start-8 3xl:col-span-5 3xl:py-[212px]">
+        <div class="lg:grid lg:grid-cols-6">
+          <div class="lg:col-span-6 3xl:col-span-5">
+            <h3
+              class="text-center lg:text-left font-trade-bold text-normal text-primary lg:pb-4 3xl:pb-[78px] uppercase"
+            >
               {{ category }} Watches
             </h3>
-            <h1 class="font-minion text-header-title pb-[132px]">{{ title }}</h1>
+            <h1
+              class="font-minion text-center lg:text-left text-header-title lg:pb-8 3xl:pb-[132px]"
+            >
+              {{ title }}
+            </h1>
           </div>
 
-          <div class="col-start-2 col-span-5">
-            <h2 class="font-minion text-[28pt] text-primary pb-[80px]">{{ priceFormatted }}</h2>
+          <div class="lg:px-[40px] lg:col-span-6 3xl:col-start-2 3xl:col-span-5">
+            <h2 class="font-minion text-[28pt] text-primary pb-[80px] text-center lg:text-left">
+              {{ priceFormatted }}
+            </h2>
             <span class="font-trade-light text-small text-secondary pb-[38px]">
               Watch {{ sku }}
             </span>
             <p class="font-minion text-header-text text-text pb-[117px] pr-[20px]">
               {{ shortDescription }}
             </p>
-            <ButtonComponent> Add to shopping bag </ButtonComponent>
+            <div class="text-center lg:text-start">
+              <ButtonComponent> Add to shopping bag </ButtonComponent>
+            </div>
           </div>
         </div>
       </div>
