5730eb84bca9d1fcb2c3b1f14265d9db81cb5921
Daniel Munoz <danimm.dev@gmail.com>
2023-08-27 23:38:08 +0200

    update carousel xs styles

--- src/components/sections/Carousel/SliderCard.vue
+++ src/components/sections/Carousel/SliderCard.vue
@@ -18,13 +18,13 @@ defineProps<{
     class="snap-center 2xl:snap-end"
   >
     <div v-bind="$attrs">
-      <div class="pt-[19px] translate-y-[-100px]">
+      <div class="md:translate-y-[-100px]">
         <img
           class="h-[40vh] object-contain md:h-[531px] w-[277px] mx-auto"
           :src="getImageUrl(mainImage)"
           :alt="subTitle"
         />
-        <div class="sm:mt-[60px] text-center">
+        <div class="md:mt-[60px] text-center">
           <h4
             class="mb-[35px] tracking-widest text-secondary text-normal font-trade-bold uppercase"
             data-test="category"
@@ -32,7 +32,7 @@ defineProps<{
             {{ category }}
           </h4>
           <h2
-            class="sm:mb-[22px] tracking-wider text-white text-title font-minion text-5xl"
+            class="sm:mb-[22px] tracking-wider text-white text-header-text sm:text-title font-minion"
             data-test="subTitle"
           >
             {{ subTitle }}
@@ -44,7 +44,7 @@ defineProps<{
             Watch {{ sku }}
           </h4>
           <h3
-            class="sm:pb-[17px] tracking-wider text-white text-[20pt] font-minion font-bold"
+            class="pb-[40px] tracking-wider text-white text-normal sm:text-[20pt] font-minion font-bold"
             data-test="priceFormatted"
           >
             {{ priceFormatted }}
