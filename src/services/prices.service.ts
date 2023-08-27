027551b5cbd1b45b2899d9492204a34a786df9da
Daniel Munoz <danimm.dev@gmail.com>
2023-08-27 11:59:21 +0200

    use only active prices

--- src/services/prices.service.ts
+++ src/services/prices.service.ts
@@ -4,7 +4,8 @@ import type { Price } from '@/types/Price'
 export class PricesService {
   static async getPrices(): Promise<Price[] | null> {
     try {
-      return Promise.resolve(prices as unknown as Price[])
+      const filteredPrices = (prices as Price[]).filter(({ isActive }) => isActive)
+      return Promise.resolve(filteredPrices)
     } catch (e) {
       // TODO: Error handler
     }
