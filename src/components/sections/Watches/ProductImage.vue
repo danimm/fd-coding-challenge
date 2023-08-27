5f75ee222d8c489f40617307bfd6ff059b05e50b
Daniel Munoz <danimm.dev@gmail.com>
2023-08-27 12:10:53 +0200

    remove props

--- src/components/sections/Watches/ProductImage.vue
+++ src/components/sections/Watches/ProductImage.vue
@@ -1,30 +1,14 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
 import { getImageUrl } from '@/composables/getImageUrl'
 
 defineOptions({ name: 'ProductImage' })
-
-const props = defineProps({
-  image: { type: String, required: true },
-  imageOverflow: { type: Boolean, default: false },
-  zoom: { type: Boolean, default: false },
-  centered: { type: Boolean, default: false }
-})
-
-const zoomPercentage = computed(() => (props.zoom ? 'h-[120%]' : 'h-[100%]'))
-
-const imageDynamicStyles = computed(() => ({
-  'translate-x-[-30px]': props.imageOverflow,
-  [zoomPercentage.value]: props.zoom,
-  'mx-auto': props.centered
-}))
+defineProps<{ image: string }>()
 </script>
 
 <template>
-  <div :class="{ 'overflow-y-hidden': imageOverflow }" class="watch bg-slider-bg h-[100%]">
+  <div class="watch bg-slider-bg h-[100%]">
     <picture>
       <img
-        :class="imageDynamicStyles"
         :src="getImageUrl(image)"
         alt="Piaget watch"
         class="bg-slider-bg h-[100%] object-cover"
